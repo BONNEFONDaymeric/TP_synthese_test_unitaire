5de804727888c4efd1ddeea94a0a771082031695
Phylante <quentin.vaudaine@univ-lyon1.fr>
2016-01-12 15:02:47 +0100

    Apres la question 1, partie 2

--- password.py
+++ password.py
@@ -9,18 +9,20 @@ def getNext(password):
     >>> getNext('bc')
     'bd'
     """
-    pwd = list(password)  #1
+    pwd = list(password)  #1 Création d'une liste qui contiendra les lettres d'un mot de passe.
     found = False
     i=len(pwd)-1
 
     while not found:
         if pwd[i] < 'z':
-           pwd[i] = chr(ord(pwd[i])+1)  #2
+           pwd[i] = chr(ord(pwd[i])+1)  #2 Si la lettre courante n'est pas un 'z', on 'incremente' à la 
+		                                # lettre suivante, sinon on passe à la lettre précedente du mot de passe.  
            found = True             
         else:
-           i = i-1 
+            pwd[i] = 'a'
+            i = i-1 
     
-    return ''.join(pwd) #3
+    return ''.join(pwd) #3 On renvoi une chaine de caracteres composée des lettes de la liste pwd(aucun séparateur).
 
 
 
